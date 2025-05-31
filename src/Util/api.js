fbb5bb40c8d693461644448a3e1ce7243fb69ba8
adilzebkhan <adiljanpk@hotmail.com>
2025-05-31 22:35:32 +0500

    updated api.js with environment url

--- src/Util/api.js
+++ src/Util/api.js
@@ -1,3 +1,4 @@
+// 1st version of api.js which uses localhost as base URL for development on local machine
 // 4) Frontend: utils/api.js
 
 // import axios from "axios";
@@ -14,10 +15,27 @@
 
 // export default api;
 
+// 2nd version of api.js in which we used direct URL that was provided during backend deployment on render
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "https://pakcar-backend.onrender.com", // Updated to Render URL
+// });
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) config.headers.Authorization = `Bearer ${token}`;
+//   return config;
+// });
+
+// export default api;
+//3rd and final version of api.js which uses environment veriable as base URL and then we set remote url in our
+// .env file like VITE_API_BASE_URL=https://pakcar-backend.onrender.com and set enviroment veriable on vercel
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://pakcar-backend.onrender.com", // Updated to Render URL
+  baseURL: import.meta.env.VITE_API_BASE_URL, // ✅ Uses environment variable
 });
 
 api.interceptors.request.use((config) => {
